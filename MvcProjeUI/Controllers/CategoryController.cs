3a2285810170b2eeee014f4b7de59fb75eac2699
Agundesli <87178579+Agundesli@users.noreply.github.com>
2022-03-24 14:48:22 +0300

    MVCProject

---
+++ MvcProjeUI/Controllers/CategoryController.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeUI.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Index()
+        {
+            return View();
+        }
+        CategoryManager categoryManager = new CategoryManager();
+        public ActionResult GetCategoryList()
+        {
+            var categoryvalues = categoryManager.GetAll();
+
+            return View(categoryvalues);
+        }
+    }
+}
